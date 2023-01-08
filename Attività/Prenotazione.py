d66db27c717498b6da9c58ec9d42480ab3846fd6
Biagioli Nicola <93011457+NicolaB01@users.noreply.github.com>
2023-01-08 23:57:15 +0100

    Add files via upload

--- Attività/Prenotazione.py
+++ Attività/Prenotazione.py
@@ -8,7 +8,6 @@ from Campo import Campo
 class Prenotazione:
     FASCE_ORARIE = list(range(8, 23))
 
-    #data_attività oggetto del tipo datetime(anno, mese, gionro, ora)
     def __init__(self, cliente, data_attività, campo, partecipanti):
         self.cliente = cliente
         self.data_attività = data_attività
@@ -16,41 +15,44 @@ class Prenotazione:
         self.partecipanti = partecipanti
         self.modificabile = True
 
-    def __eq__(self, other):
-        return isinstance(other,Prenotazione) and self.cliente == other.cliente and self.data_prenotazione == other.data_prenotazione and self.campo == other.campo and self.partecipanti == other.partecipanti
+    def __str__(self):
+        return f"""
+            cliente         :{self.cliente}
+            data attività   :{self.data_attività}
+            campo           :{self.campo}
+            partecipanti    :{self.partecipanti}
+            """
 
-    #controlla che nel campo scelto non vi siano già altre prenotazioni per quella stessa fascia oraria poi salva la nuova prenotazione e la aggiunge alle prenotazioni del cliente
+    def __eq__(self, other):
+        return isinstance(other,
+                          Prenotazione) and self.cliente == other.cliente and self.data_prenotazione == other.data_prenotazione and self.campo == other.campo and self.partecipanti == other.partecipanti
+
     @classmethod
     def prenota_campo(cls, nome_campo, data_attività, partecipanti):
         campo = Campo.cerca_campo(nome_campo)
         prenotazioni = []
         if os.path.getsize(campo.path_prenotazioni) != 0:
-            if campo.controlla_disponibilità(data_attività):
-                with open(campo.path_prenotazioni,"rb+") as f:
-                    prenotazioni = pickle.load(f)
-                    prenotazioni.append(Prenotazione(Cliente.get_account_connesso(), data_attività, nome_campo, partecipanti))
-                    pickle.dump(prenotazioni,f,pickle.HIGHEST_PROTOCOL)
+            with open(campo.path_prenotazioni, "rb") as f:
+                prenotazioni = pickle.load(f)
+                prenotazioni.append(Prenotazione(Cliente.get_account_connesso(), data_attività, nome_campo, partecipanti))
+
+            with open(campo.path_prenotazioni, "wb") as f:
+                pickle.dump(prenotazioni, f, pickle.HIGHEST_PROTOCOL)
         else:
             with open(campo.path_prenotazioni, "wb") as f:
-                nuova_prenotazione = Prenotazione(Cliente.get_account_connesso(), data_attività, nome_campo, partecipanti)
-                prenotazioni.append(nuova_prenotazione)
-                print(type(nuova_prenotazione))
+                prenotazioni.append(Prenotazione(Cliente.get_account_connesso(), data_attività, nome_campo, partecipanti))
+
                 pickle.dump(prenotazioni, f, pickle.HIGHEST_PROTOCOL)
 
     @classmethod
-    def get_prenotazioni(cls, campo):
-        if os.path.getsize(campo.path_prenotazioni) != 0:
-            with open(campo.path_prenotazioni, "rb+") as f:
-                return pickle.load(f)
-
-    def filtra_prenotazione(self):
+    def filtra_prenotazione(cls, attività, giorno, mese, anno, ora_inizio):
         pass
 
 
     def aggiugni_partecipante(self):
         pass
 
-    #TODO da fare mi raddopia le prenotazioni invece di toglierle
+    # TODO da fare mi raddopia le prenotazioni invece di toglierle
     def elimina_prenotazione(self):
         with open(Campo.PATH_INFOCAMPI, "rb") as f:
             campi = pickle.load(f)
@@ -66,6 +68,13 @@ class Prenotazione:
                         pickle.dump(prenotazioni, f, pickle.HIGHEST_PROTOCOL)
 
     @classmethod
+    def get_prenotazioni(cls, nome_campo):
+        campo = Campo.cerca_campo(nome_campo)
+        if os.path.getsize(campo.path_prenotazioni) != 0:
+            with open(campo.path_prenotazioni, "rb+") as f:
+                return pickle.load(f)
+
+    @classmethod
     def cerca_prenotazione(cls, cliente):
         with open(Campo.PATH_INFOCAMPI, "rb") as f:
             campi = pickle.load(f)
@@ -77,5 +86,4 @@ class Prenotazione:
                         for j in range(len(prenotazioni)):
                             if prenotazioni[i].cliente.nome == cliente:
                                 return prenotazioni[i]
-
 
