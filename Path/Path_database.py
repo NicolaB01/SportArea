2c200ab4826b8974e0e07d1a1f7449b0ff98bb7d
Biagioli Nicola <93011457+NicolaB01@users.noreply.github.com>
2023-01-24 11:11:45 +0100

    Delete Path directory

--- Path/Path_database.py
+++
@@ -1,6 +0,0 @@
-import os
-
-PATH_INFO_CAMPI = os.path.join("DataBase", "Campi", "InfoCampi.txt")
-PATH_RICEVUTE = os.path.join("DataBase", "ricevute.txt")
-PATH_INFO_CLIENTI = os.path.join("DataBase","Utenti", "Credenziali.txt")
-PATH_ACCOUNT_CONNESSO = os.path.join("DataBase","Utenti", "Account_connesso.txt")
