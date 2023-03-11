42e725a6aaf14de08f62a535a15a7d03fc01dbb3
Biagioli Nicola <93011457+NicolaB01@users.noreply.github.com>
2023-03-11 15:09:54 +0100

    Add files via upload

--- Path/Path_database.py
+++ Path/Path_database.py
@@ -4,4 +4,5 @@ PATH_INFO_CAMPI = os.path.join("DataBase", "Campi", "InfoCampi.txt")
 PATH_RICEVUTE = os.path.join("DataBase", "ricevute.txt")
 PATH_INFO_CLIENTI = os.path.join("DataBase","Utenti", "Credenziali.txt")
 PATH_ACCOUNT_CONNESSO = os.path.join("DataBase","Utenti", "Account_connesso.txt")
-PATH_BACKUP = os.path.join("DataBase","Backup.txt")
+PATH_BACKUP = os.path.join("DataBase","Backup")
+#todo fai in modo che la dir database e tutta sta merda se crei da sola
