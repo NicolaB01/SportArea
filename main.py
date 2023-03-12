f30db1cc46ab8ccfa7d08afd21750804dd054520
Biagioli Nicola <93011457+NicolaB01@users.noreply.github.com>
2023-03-12 19:47:12 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ from Viste.Accesso.Home import Home
 schedule.every(5).seconds.do(Gestore_prenotazione.controlla_scadenza_prenotazioni)
 schedule.every().day.at("23:30").do(Backup.esegui_backup)
 
-def controlli():
+#Questo metodo effettua i controlli ogni 5 secondi per la scadenza delle prenotazioni e ogni giorno alle 23:30
+#effettua il backup del sistema.
+def pianificazione():
     while not closing:
         schedule.run_pending()
         time.sleep(1)
@@ -22,7 +24,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     mainwindow = Home()
     mainwindow.show()
-    threading.Thread(target=controlli).start()
+    threading.Thread(target=pianificazione).start()
     try:
         sys.exit(app.exec())
     except:
